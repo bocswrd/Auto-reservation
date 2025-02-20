40610a1783f4dad48f699053cfc61fd85808bb6f
menuki <menu3121@icloud.com>
2025-02-20 23:41:08 +0900

    add

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# 仮想環境のアクティベート
+* 仮想環境のアクティベート
 ``` bash
 venv\Scripts\activate
+```
+
+* インストール済みバッケージとバージョンの出力
+``` bash
+pip freeze > requirements.txt
+```
+* 出力したrequirements.txtを一括インストール
+``` bash
+pip install -r requirements.txt
 ```
--- airdo_reserve.py
+++ airdo_reserve.py
@@ -15,41 +15,52 @@ import time
 # Chromeのオプション設定
 chrome_options = Options()
 # chrome_options.add_argument("--headless")  # ヘッドレスモード（GUIなし）
+chrome_options.headless = False  # これでブラウザが表示される
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.headless = False  # これでブラウザが表示される
 
 # Chromeドライバーのセットアップ
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
+url = "https://www.airdo.jp/"
 try:
-    # AIRDOのチケット検索ページにアクセス
-    url = "https://www.airdo.jp/"  # 実際のURLに変更してください
     driver.get(url)
 
-    # ボタンが表示されるまで待機
+    # 航空券を検索する
     wait = WebDriverWait(driver, 10)
     search_button = wait.until(EC.element_to_be_clickable((By.ID, "ticket-search")))
+    # 「出発地」・「到着地」を指定
+    Select(driver.find_element(By.NAME, "from")).select_by_value("HND")
+    Select(driver.find_element(By.NAME, "to")).select_by_value("SPK")
 
-    # 「出発地」を指定
-    departure_select = Select(driver.find_element(By.NAME, "from"))
-    departure_select.select_by_value("HND")
-
-    # 「到着地」を指定
-    departure_select = Select(driver.find_element(By.NAME, "to"))
-    departure_select.select_by_value("SPK")
-
-
-    # ボタンをクリック
     search_button.click()
 
-    # ページ遷移を待つ
+    # HACK: webdriverwaitで待機する
     time.sleep(5)  # 画面遷移待ち（適宜調整）
 
     # 遷移後のページの情報を取得
-    # print(driver.page_source)
-    print('complete')
+    element = driver.find_element(By.XPATH, '//*[@id="tabSpS2"]/div/ul/li[9]')
+    print(element.get_attribute("textContent"))
+    print(element.text)
+    # クリック前のクラスを取得
+    before_class = element.get_attribute("class")
+    print(f"クリック前: {before_class}")
+
+    # クリックを実行
+    # element.click()
+    # driver.execute_script("arguments[0].click();", element)
+    actions = ActionChains(driver)
+    actions.move_to_element(element).click().perform()
+    driver.execute_script("document.querySelector('.rsv-select .table-calendar td:not(.select-disable), .list-calendar li:not(.select-disable)').click();")
+
+    # クリック後のクラスを取得
+    after_class = element.get_attribute("class")
+    print(f"クリック後: {after_class}")
+
+
+    button = driver.find_element(By.CSS_SELECTOR, "#form-cart button[type='submit']")
+    button.click()
 
 finally:
     driver.quit()
