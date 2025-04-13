f48abdc6cc7d3d76143f91f26eca7c6b2ffc856a
bocswrd <menu3121@icloud.com>
2025-04-14 01:00:38 +0900

    clean: apply Page Object Pattern

--- src/airdo_reserve_playwright.py
+++ src/airdo_reserve_playwright.py
@@ -1,6 +1,7 @@
 import os
 import re
 import time
+from pages.ReservationFormPage import ReservationFormPage
 from dotenv import load_dotenv
 from playwright.sync_api import Playwright, sync_playwright, expect
 
@@ -12,7 +13,7 @@ def run(playwright: Playwright) -> None:
     Args:
         playwright (Playwright): Playwrightのインスタンス
     """
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
     # Airdoの予約サイトにアクセス
@@ -21,30 +22,26 @@ def run(playwright: Playwright) -> None:
     page.locator('select[name="from"]').select_option("HND")
     page.get_by_role("button", name="検索する").click()
     # 検索結果から航空券を選択
-    page.get_by_role("row", name="ADO 011 763 06:55 - 08:25 ◯ ¥").get_by_role(
-        "cell"
-    ).nth(1).click()
+    # page.get_by_role("row", name="ADO 011 763 06:55 - 08:25 ◯ ¥").get_by_role(
+    #     "cell"
+    # ).nth(1).click()
     page.get_by_role("cell", name="△ ¥26,320").click()
     page.locator("#form-cart").get_by_role("button", name="次へ").click()
     page.get_by_role("button", name="ログインせずに次へ進む").click()
     # 予約者情報の入力
-    page.get_by_role("textbox", name="例）サトウ").fill(os.getenv("LAST_NAME"))
-    page.get_by_role("textbox", name="例）イチロウ").fill(
-        os.getenv("FIRST_NAME")
+    reservation_form_page = ReservationFormPage(page)
+    reservation_form_page.enter_individual_to_form(
+        os.getenv("LAST_NAME"),
+        os.getenv("FIRST_NAME"),
+        os.getenv("AGE"),
+        os.getenv("E_MAIL"),
+        os.getenv("TEL_NUMBER"),
     )
-    page.locator("#age-1").fill(os.getenv("AGE"))
-    page.get_by_role("textbox", name="例）sample@airdo.jp").fill(
-        os.getenv("E_MAIL")
-    )
-    page.locator("#input-conf-req").fill(os.getenv("E_MAIL"))
-    page.get_by_role("textbox", name="例）09012345678").fill(
-        os.getenv("TEL_NUMBER")
-    )
-    page.get_by_role("button", name="確認画面に進む").click()
+    reservation_form_page.submit_form()
     # 予約の確定
     page.locator("label").click()
     page.screenshot(path="screenshot.png", full_page=True)
-    page.get_by_role("button", name="予約する").click()
+    # page.get_by_role("button", name="予約する").click()
 
 
 start = time.time()
