e870320abdc37abf211d84c70fe3782cd22e14da
bocswrd <menu3121@icloud.com>
2025-05-15 01:21:00 +0900

    clean: ライブラリ更新スクリプトの処理をmainでラッピング

--- update_library.sh
+++ update_library.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-echo "Updating library..."
+main ()
+{
+    echo "Updating library..."
 
-# リポジトリのルートディレクトリに移動
-cd "$(git rev-parse --show-toplevel)"
+    # リポジトリのルートディレクトリに移動
+    cd "$(git rev-parse --show-toplevel)"
 
-# 仮想環境がアクティブな場合は非アクティブ化
-if [[ -n "${VIRTUAL_ENV:-}" ]]; then
-    echo "Deactivating virtual environment..."
-    # NOTE: 拡張機能が原因でエラーが発生する可能性があるが、無視して問題ない
-    deactivate || true
-fi
+    # 仮想環境がアクティブな場合は非アクティブ化
+    if [[ -n "${VIRTUAL_ENV:-}" ]]; then
+        echo "Deactivating virtual environment..."
+        # NOTE: 拡張機能が原因でエラーが発生する可能性があるが、無視して問題ない
+        deactivate || true
+    fi
 
-# 仮想環境を再構築
-echo "Rebuilding virtual environment..."
-rm -rf .venv/
-python -m venv .venv
-source .venv/Scripts/activate
-pip install -r requirements.txt
+    # 仮想環境を再構築
+    echo "Rebuilding virtual environment..."
+    rm -rf .venv/
+    python -m venv .venv
+    source .venv/Scripts/activate
+    pip install -r requirements.txt
 
-echo "Library updated successfully!"
+    echo "Library updated successfully!"
+}
+
+main
